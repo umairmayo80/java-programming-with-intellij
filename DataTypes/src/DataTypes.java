dea548e0b479b35cf26bb006dced4241e56e6314
umair.tariq <umair.tariq@techno-soft.com>
2023-06-06 13:17:03 +0500

    Data Types Done

---
+++ DataTypes/src/DataTypes.java
@@ -0,0 +1,24 @@
+public class DataTypes{
+    public static void main(String[] args)
+    {
+        // integer types
+        byte aSingleByte = 100;
+        // short, int, long
+
+        // float, double, by default it is double for decimal numbers
+
+        // boolen
+        // char
+        char copyrightSymbol = '\u00A9';
+        System.out.println(copyrightSymbol);
+
+        // implicit converion
+        double number1 = 5.8;
+        // int number2 = number1; // it  will throw an error as no implicit conversation from double to int due to possible data loss
+
+        // explicit
+        int num3 = (int) number1;
+        System.out.println(num3);
+
+    }
+}
