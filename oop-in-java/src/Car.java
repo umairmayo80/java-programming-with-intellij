71dd617706624ad4910e92a4aa7e2d6ba1c23d80
umair.tariq <umair.tariq@techno-soft.com>
2023-06-08 14:07:37 +0500

    Car Class with Constructor, Setter, and Getters functions

--- oop-in-java/src/Car.java
+++ oop-in-java/src/Car.java
@@ -1,46 +1,58 @@
-public class Car{
-    public static void main(String[] args){
-        CarClass car = new CarClass("honda","hv12",1300);
+public class Car {
+    public static void main(String[] args) {
+        CarClass car = new CarClass("Honda", "HV12", 1300);
         System.out.println(car);
     }
 }
 
-
-
-
-class CarClass{
+class CarClass {
     private String brand;
     private String model;
     private float price;
 
     // Default Constructor
-    CarClass(){
+    CarClass() {
         this.brand = "";
         this.model = "";
         this.price = 0;
     }
 
     // Parameterized Constructor
-    CarClass(String brand, String model, float price){
-        setCar(brand,model,price);
+    CarClass(String brand, String model, float price) {
+        setCar(brand, model, price);
     }
 
-
-    // setter function
-    public void setBrand(String brand){
+    // Setter functions
+    public void setBrand(String brand) {
         this.brand = brand;
     }
-    public void setModel(String model){
+
+    public void setModel(String model) {
         this.model = model;
     }
-    public void setPrice(float price){
+
+    public void setPrice(float price) {
         this.price = price;
     }
 
-    public void setCar(String brand, String model, float price){
+    // Set all car attributes using setter functions
+    public void setCar(String brand, String model, float price) {
         setBrand(brand);
         setModel(model);
         setPrice(price);
+    }
+
+    // Getter functions
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public float getPrice() {
+        return price;
     }
 
     @Override
